6028ed2733cd0d5eb41f270969be8b6fedf7e7b5
DarkElvenAngel <5490359-DarkElvenAngel@users.noreply.gitlab.com>
2022-01-17 23:11:51 -0500

    ADD: Void Linux

---
+++ OS/void/argonone_stage3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This script is written for VOID LINUX to run during stage 3
+# and should be called by /etc/rc.shutdown 
+
+REBOOT=$(stat -c "%a" /run/runit/reboot)
+[[ $((0$REBOOT & 0100)) -ne 0 ]] && /usr/sbin/shutdown_argonone poweroff
+exit 0
