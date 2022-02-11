8be0c4760f6afd6b4d9a50c64cec17487f522a65
DarkElvenAngel <5490359-DarkElvenAngel@users.noreply.gitlab.com>
2022-02-11 12:10:02 -0500

    FIX: lakka shutdown.sh not used as #31

--- OS/lakka/pkg_install.sh
+++ OS/lakka/pkg_install.sh
@@ -12,31 +12,24 @@ F_INSTALL()
     echo "OK"
     echo "INFO:  Installing"
     [[ -d /storage/sbin ]] || mkdir /storage/sbin
+    [[ -d /storage/.work ]] || mkdir /storage/.work
+    [[ -d /storage/.systemd ]] || mkdir /storage/.systemd
+    mount -t overlay overlay -o lowerdir=/lib/systemd,upperdir=/storage/.systemd,workdir=/storage/.work /lib/systemd
     install build/argononed /storage/sbin/argononed 2>/dev/null || { echo "ERROR:  Cannot install argononed"; exit 1;}
-    install -m 4755 build/argonone-cli /storage/sbin/argonone-cli 2>/dev/null || { echo "ERROR:  Cannot install argonone-cli"; exit 1;}
-    install build/argonone-shutdown /storage/sbin/argonone-shutdown 2>/dev/null || { echo "ERROR:  Cannot install argonone-shutdown"; exit 1;}
+    install -m 0755 build/argonone-cli /storage/sbin/argonone-cli 2>/dev/null || { echo "ERROR:  Cannot install argonone-cli"; exit 1;}
+    install -D build/argonone-shutdown /lib/systemd/system-shutdown/argonone-shutdown 2>/dev/null || { echo "ERROR:  Cannot install argonone-shutdown"; exit 1;}
     [[ -f /storage/.profile ]] || cp /etc/profile /storage/.profile
-    grep /storage/sbin/ /storage/.profile &>/dev/null|| echo "export PATH=/storage/sbin/:$PATH" >> /storage/.profile
+    grep /storage/sbin/ /storage/.profile &>/dev/null || echo "export PATH=/storage/sbin/:$PATH" >> /storage/.profile
     # Change path form /usr/sbin to /storage/sbin
     sed -i "s/usr/storage/g" OS/_common/argononed.service
     install OS/_common/argononed.service /storage/.config/system.d/argononed.service || { echo "ERROR:  Cannot install argononed.service"; exit 1;}
+    install OS/lakka/argonone-shutdown.service /storage/.config/system.d/argonone-shutdown.service || { echo "ERROR:  Cannot install argononed-shutdown.service"; exit 1;}
     systemctl daemon-reload
-    systemctl enable argononed
+    systemctl enable argononed &>/dev/null
+    systemctl enable argonone-shutdown &>/dev/null
     mount -o remount,rw /flash
     install build/argonone.dtbo /flash/overlays
     sh OS/_common/setup-overlay.sh /flash/config.txt
     mount -o remount,ro /flash
-    if [[ -f /storage/.config/shutdown.sh ]]
-    then
-    {
-        echo "/storage/sbin/argonone-shutdown \$1" >> /storage/.config/shutdown.sh
-    } else {
-        cat > /storage/.config/shutdown.sh << END_SCRIPT
-#!/bin/bash
-/storage/sbin/argonone-shutdown \$1
-END_SCRIPT
-        chmod +x /storage/.config/shutdown.sh
-    }
-    fi
     [[ -r /dev/i2c-1 ]] ||  echo "reboot required" && systemctl start argononed
 }
