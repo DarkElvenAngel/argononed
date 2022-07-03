d45c1001ff3796a04db5c50c5b0e7ba69b98068f
DarkElvenAngel <>
2022-07-02 23:14:56 -0400

    FIX: missing $

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set TARGET_DISTRO=$1
-[[ -n GIT_BRANCH ]] && { git checkout remotes/origin/$GIT_BRANCH &> /dev/null || exit 1; }
+[[ -n $GIT_BRANCH ]] && { git checkout remotes/origin/$GIT_BRANCH &> /dev/null || exit 1; }
 ./configure
 echo "CFLAGS+=-largp -static" >> makefile.conf
 unset TARGET_DISTRO
